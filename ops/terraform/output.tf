2e00332ecc5bba778653858331d77c34b4dd76a3
BPhuc246 <nhbphuc2006@gmail.com>
2026-07-25 19:33:57 +0700

    feat: terraform

---
+++ ops/terraform/output.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.my_terraform_public_ip.ip_address
+}
+
+output "ssh_command" {
+  value = "ssh phucadmin@${azurerm_public_ip.my_terraform_public_ip.ip_address}"
+}
